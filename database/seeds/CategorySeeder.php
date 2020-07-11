0f34a1de44c8dc556264b83c6ebd7b7803fd35a0
Kamil Machanski <kamilmachanski@gmail.com>
2020-07-11 17:40:26 +0200

    create Category tree seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Category;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,64 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $node = Category::create([
+            'name' => 'Moda',
+            'children' => [
+                [
+                    'name' => 'Ona',
+                    'children' => [
+                        ['name' => 'Buty damskie'],
+                        ['name' => 'Odzież damska'],
+                        ['name' => 'Sandały'],
+                    ],
+                ], [
+                    'name' => 'On',
+                    'children' => [
+                        ['name' => 'Buty męskie'],
+                        ['name' => 'Odzież męska'],
+                        ['name' => 'Zegarki męskie'],
+                    ],
+                ], [
+                    'name' => 'Dziecko',
+                    'children' => [
+                        ['name' => 'Buty dziecięce'],
+                        ['name' => 'Odzież dziecięca'],
+                        ['name' => 'Dresy dziecięce'],
+                    ],
+                ],
+            ],
+        ]);
+
+
+        $node = Category::create([
+            'name' => 'Elektronika',
+            'children' => [
+                [
+                    'name' => 'Komputery',
+                    'children' => [
+                        ['name' => 'Laptopy'],
+                        ['name' => 'PC'],
+                        ['name' => 'Podzespoły komputerowe'],
+                    ],
+                ], [
+                    'name' => 'AGD',
+                    'children' => [
+                        ['name' => 'Do domu'],
+                        ['name' => 'Odkurzacze'],
+                        ['name' => 'Kuchnia'],
+                    ],
+                ], [
+                    'name' => 'Telefony i akcesoria',
+                    'children' => [
+                        ['name' => 'Smartfony'],
+                        ['name' => 'Smartwatche'],
+                        ['name' => 'Tablety'],
+                    ],
+                ],
+            ],
+        ]);
+
+        Category::fixTree();
+
     }
 }
